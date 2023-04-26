d6b26c857665d02a1dda99f68df011c1177769a3
YoRHazzz <1245469389@qq.com>
2023-04-26 18:33:35 +0800

    start: 🎉 v0.1.2

--- TorchExpManager/DataLoaderWrapper/XyDataLoaderWrapper.py
+++
@@ -1,6 +0,0 @@
-from .BaseDataLoaderWrapper import BaseDataLoaderWrapper
-
-
-class XyDataLoaderWrapper(BaseDataLoaderWrapper):
-    def split_iter_data(self, iter_data):
-        self.iter_data['x'], self.iter_data['y'] = iter_data
